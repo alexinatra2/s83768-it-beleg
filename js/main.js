b91a7cbbf2cc50558a67e224f7b8340b6f94d69c
alexinatra2 <a.holzknecht@gmx.de>
2023-04-13 23:21:53 +0200

    implemented request functions for web-quiz APIs without functioning credentials

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+const api_base_url = "https://irene.informatik.htw-dresden.de:8888/api/quizzes/";
+
+// utility for checking a radio button option
 function check(event) {
   var elem = event.target;
   var optionsElem = document.getElementById("options");
@@ -9,4 +12,45 @@ function check(event) {
   elem.classList.add("selected");
   var inputElem = elem.querySelector("input");
   inputElem.checked = true;
+}
+
+// get a question from the web-quizzes api
+async function get_question() {
+  const response = await fetch(
+    api_base_url + "2",
+    {
+      method: "GET",
+      credentials: "include",
+    },
+  );
+  return await response.json();
+}
+
+
+// solve a question from the web-quizzes api
+async function solve_question(solution) {
+  const response = await fetch(
+    api_base_url + "2/solve",
+    {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(solution),
+    },
+  );
+  return await response.json();
+}
+
+// get all quiz questions that have been completed so far
+async function completed_questions() {
+  const response = await fetch(
+    api_base_url + "completed",
+    {
+      method: "GET",
+      credentials: "include",
+    }
+  );
+  return await response.json();
 }
