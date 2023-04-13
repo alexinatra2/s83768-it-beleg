d11c67ec9aa4ce38d6fc1f4336400594b9205c2e
alexinatra2 <a.holzknecht@gmx.de>
2023-04-13 22:46:40 +0200

    added completed request function to web-quiz-irene.bash

--- web-quiz-irene.bash
+++ web-quiz-irene.bash
@@ -10,3 +10,8 @@ function solve() {
     "https://irene.informatik.htw-dresden.de:8888/api/quizzes/$1/solve"
     --data $(echo "[$2]") | python -m json.tool
 }
+
+function completed() {
+  curl --user test@gmail.com:secret -X GET \
+    "https://irene.informatik.htw-dresden.de:8888/api/quizzes/completed" | python -m json.tool
+}
