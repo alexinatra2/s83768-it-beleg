6b7a35b0da5da213cd52ef6ad1a0e12ae8d2d4a2
alexinatra2 <a.holzknecht@gmx.de>
2023-04-13 17:44:31 +0200

    added utility script for querying web-quiz running on https://irene.informatik.htw-dresden.de:8888

---
+++ web-quiz-irene.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+function get() {
+    curl --user test@gmail.com:secret -X GET \
+      "https://irene.informatik.htw-dresden.de:8888/api/quizzes/$1" | python -m json.tool
+}
+
+function solve() {
+  curl --user test@gmail.com:secret -X POST -H 'Content-Type: application/json' \
+    "https://irene.informatik.htw-dresden.de:8888/api/quizzes/$1/solve"
+    --data $(echo "[$2]") | python -m json.tool
+}
